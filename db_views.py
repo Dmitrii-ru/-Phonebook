c6d48142ba3377878dd7a79c9737de31be987b5d
dim <nochev1@mail.ru>
2023-08-19 05:46:50 +0300

    first

--- db_views.py
+++ db_views.py
@@ -2,9 +2,9 @@ import json
 
 
 def move_bad_data_to_file(source_file):
-    with open(source_file, 'r', encoding='utf-8') as file:
+    with open(f'database/{source_file}', 'r', encoding='utf-8') as file:
         content = file.read()
-    with open('bad_data.txt', 'w', encoding='utf-8') as bad_file:
+    with open('database/bad_data.txt', 'w', encoding='utf-8') as bad_file:
         bad_file.write(content)
 
 
@@ -20,20 +20,29 @@ def db_manager(method=None, obj=None):
 
     try:
         if method == 'r':
-            with open('phonebook.txt', 'r') as file:
+            with open('database/phonebook.txt', 'r') as file:
                 data = json.load(file)  # encode в json
                 return data
         elif method == 'w':
             existing_data = db_manager(method='r', obj=obj)  # Загрузить существующие данные
+            if obj in existing_data:
+                return print('Такая запись уже есть ')
+
             if existing_data is None:  # Смотрим пустая ли бд
-                existing_data = []  # Добавляем список
+                existing_data = []  # Приводим файл в формат list
             existing_data.append(obj)  # Добавить новый словарь
-            with open('phonebook.txt', 'w') as file:
-                json.dump(existing_data, file)  # encode в json, добавляем в бд
+
+            try:
+                with open('database/phonebook.txt', 'w') as file:
+                    json.dump(existing_data, file, indent=2, ensure_ascii=False)  # Загрузить существующие данные
+                print("Запись успешно добавлена.")
+            except OSError as e:  # Обработка в случаи не возможности записи
+                print(f"ERROR : Ошибка записи в файл: {e}")
+
 
     except (FileNotFoundError, json.JSONDecodeError):  # Если не получилось decoded json
-        print('База данных отсутствует или была повреждена, создаем новую')
-        with open('phonebook.txt', 'w', encoding='utf-8') as clean_file:  # Open or create and open
+        print('ERROR :База данных отсутствует или была повреждена, создаем новую')
+        with open('database/phonebook.txt', 'w', encoding='utf-8') as clean_file:  # Open or create and open
             move_bad_data_to_file('phonebook.txt')  # Переносим поврежденный данные в спец. файл
             clean_file.truncate(0)  # Очистить phonebook.txt
             json.dump([], clean_file)  # Создаем пустой список
@@ -44,26 +53,5 @@ def db_read():
 
 
 def db_create(new_record):
-    db_manager(method='w', obj=new_record)
+    db_manager(method='w', obj=new_record)  # Создаем новую запись
 
-
-# Функция для вывода записей из справочника
-# def display_records():
-#     if os.path.exists(DATA_FILE):
-#         with open(DATA_FILE, "r") as file:
-#
-#             print(*
-#                   [
-#                       centered_string('Фамилия'),
-#                       centered_string('Имя'),
-#                       centered_string('Отчество'),
-#                       centered_string('Организация'),
-#                       centered_string('Телефон рабочий'),
-#                       centered_string('Телефон личный'),
-#                   ]
-#                   )
-#             for line in file:
-#                 parts = line.strip().split(',')
-#                 print(*parts)
-#     else:
-#         print("Справочник пуст")
